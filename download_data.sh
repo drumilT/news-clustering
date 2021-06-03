1262edd45525dabb15d9fbdbd1830c79a72c978c
drumilT <drumiltrivedi@gmail.com>
2021-06-04 01:53:45 +0530

    copy data into sub folder

--- download_data.sh
+++ download_data.sh
@@ -3,3 +3,4 @@ wget -P dataset ftp://"ftp.priberam.pt|anonymous"@ftp.priberam.pt/SUMMAPublic/Co
 wget -P dataset ftp://"ftp.priberam.pt|anonymous"@ftp.priberam.pt/SUMMAPublic/Corpora/Clustering/2018.0/dataset/dataset.test.json
 wget -P dataset ftp://"ftp.priberam.pt|anonymous"@ftp.priberam.pt/SUMMAPublic/Corpora/Clustering/2018.0/dataset-tok-ner/clustering.dev.json
 wget -P dataset ftp://"ftp.priberam.pt|anonymous"@ftp.priberam.pt/SUMMAPublic/Corpora/Clustering/2018.0/dataset-tok-ner/clustering.test.json
+cp dataset/* dataset/og_data
